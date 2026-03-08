79de0406acfef9793c9a3347ddb60a7d713327cd
Gibsongf <maccgiiibson@gmail.com>
2026-03-08 19:54:06 -0300

    header layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
-export const Header = () => {
+import ToothIcon from "./svg/ToothIcon";
+import UserIcon from "./svg/UserIcon";
+import style from "./Header.module.css";
+import { Link } from "react-router-dom";
+const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="servicos"> Serviços</NavLink>
-        </li>
-        <li>
-          <NavLink to="clientes"> Clientes</NavLink>
-        </li>
-        <li>
-          <NavLink to="clinicas"> Clínicas</NavLink>
-        </li>
-        <li>
-          <NavLink to="produtos"> Produtos</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className={style.container}>
+      <div className="icon-container">
+        <ToothIcon />
+        <p>Manejamento Protético</p>
+      </div>
+      <div className="icon-container">
+        <UserIcon />
+        <Link>Minha Conta</Link>
+      </div>
+    </div>
   );
 };
+
+export default Header;
