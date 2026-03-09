e35efa696344512e6fa58fca6142ca53f65f3944
Gibsongf <maccgiiibson@gmail.com>
2026-03-08 22:53:19 -0300

    remove nav from css module, easier without it on nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,47 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import style from "./Nav.module.css";
+import "./Nav.css";
 import ToothIcon from "./svg/ToothIcon";
 import ClientIcon from "./svg/ClientIcon";
 import ClinicIcon from "./svg/ClinicIcon";
 import ServiceIcon from "./svg/ServiceIcon";
 
-const Links = ({ text, path }) => {
-  const [iconColor, setIconColor] = React.useState("#1F2937");
-  const activeStyle = ({ isActive }) => {
-    if (isActive) {
-      setIconColor("#fff");
-      return {
-        color: "#FFF",
-        backgroundColor: "#0D9488",
-      };
-    } else {
-      setIconColor("#1F2937");
-    }
-  };
-  const Icon = {
-    "/servicos": <ServiceIcon color={iconColor} />,
-    "/clientes": <ClientIcon color={iconColor} />,
-    "/clinicas": <ClinicIcon color={iconColor} />,
-    "/produtos": <ToothIcon color={iconColor} />,
-  };
-  return (
-    <li>
-      <NavLink style={activeStyle} className={style.link} to={path}>
-        {Icon[path]}
-        <p>{text}</p>
-      </NavLink>
-    </li>
-  );
-};
 const Nav = () => {
   return (
-    <nav className={style.container}>
+    <nav className="container">
       <ul>
-        <Links text="Serviços" path="/servicos" />
-        <Links text="Clientes" path="/clientes" />
-        <Links text="Clinicas" path="/clinicas" />
-        <Links text="Produtos" path="/produtos" />
+        <li>
+          <NavLink className="link" to="/servicos">
+            <ServiceIcon color=" #1F2937" />
+            <p>Serviços</p>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="link" to="/clientes">
+            <ClientIcon color=" #1F2937" />
+            <p>Clientes</p>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="link" to="/clinicas">
+            <ClinicIcon color=" #1F2937" />
+            <p>Clínicas</p>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="link" to="/produtos">
+            <ToothIcon color="#1F2937" />
+            <p>Produtos</p>
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
